713204e9e4335a260a84d1d2d3a01f92baf91e9a
matheus emanuelsc <matheus.emanuelsc@gmail.com>
2025-02-26 18:55:16 -0300

    partial views

---
+++ Portfolio/src/Portfolio/Models/HomeIndexViewModel.cs
@@ -0,0 +1,6 @@
+namespace Portfolio.Models;
+
+public class HomeIndexViewModel
+{
+    public IEnumerable<ProjetoViewModel> Projetos { get; set; }
+}
---
+++ Portfolio/src/Portfolio/Models/ProjetoViewModel.cs
@@ -0,0 +1,9 @@
+namespace Portfolio.Models;
+
+public class ProjetoViewModel
+{
+    public string Titulo { get; set; } = string.Empty;
+    public string Descricao { get; set; }= string.Empty;
+    public string ImgUrl { get; set; }= string.Empty;
+    public string Link { get; set; }= string.Empty;
+}
